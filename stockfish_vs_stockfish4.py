8351b19533ff8e06c1a7dcdbc1510440ecec2528
lorand <lorand77@users.noreply.github.com>
2026-02-11 21:26:33 +0100

    fixed bug

--- stockfish_vs_stockfish4.py
+++ stockfish_vs_stockfish4.py
@@ -4,48 +4,36 @@ import chess.pgn
 import chess.engine
 import random
 
-logging.basicConfig(
-    filename="uci.log",
-    level=logging.DEBUG,
-    filemode="w"
-)
+# logging.basicConfig(
+#     filename="uci.log",
+#     level=logging.DEBUG,
+#     filemode="w"
+# )
 
-board = chess.Board("1k6/3r4/8/8/8/6R1/8/4K3 w - - 99 1")
+#board = chess.Board("1k6/3r4/8/8/8/6R1/8/4K3 w - - 98 1")
+board = chess.Board()
 engine = chess.engine.SimpleEngine.popen_uci(r"C:\Users\lorand\Programs\stockfish\stockfish-windows-x86-64-avx2.exe")
 engine.configure({"Threads": 8, "Hash": 1024})
 engine.configure({"SyzygyPath": r"C:\Users\lorand\Programs\syzygy"})
 
 move_number = 1
 score = []
-while not board.is_game_over():
+while not board.is_game_over(claim_draw=True):
     print(f"Move {move_number}")
     # white (Stockfish) makes a move
-    engine_result = engine.analyse(board, chess.engine.Limit(time=0.1))
+    engine_result = engine.analyse(board, chess.engine.Limit(time=1))
     best_move = engine_result["pv"][0]
-    #print(engine_result['score'])
     score.append(engine_result['score'].white().score())
     board.push(best_move)
-    # engine.quit()
-    # exit()
-    if board.is_game_over():
+    if board.is_game_over(claim_draw=True):
         break
     # black (Stockfish) makes a move
-    engine_result = engine.analyse(board, chess.engine.Limit(time=0.1))
+    engine_result = engine.analyse(board, chess.engine.Limit(time=1))
     best_move = engine_result["pv"][0]
     board.push(best_move)
     move_number += 1
 
 game = chess.pgn.Game.from_board(board)
 print(game.mainline_moves())
-
-import matplotlib.pyplot as plt
-
-plt.plot(score)
-plt.xlabel('Move Number')
-plt.ylabel('Score (White perspective)')
-plt.title('Game Score Evolution')
-plt.axhline(y=0, color='r', linestyle='--', alpha=0.5)
-plt.grid(True)
-plt.show()
 
 engine.quit()
