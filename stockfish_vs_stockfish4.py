fd6acf86a4209d1c8a50b3745b580011c9e5a60e
lorand <lorand77@users.noreply.github.com>
2026-02-11 18:34:01 +0100

    add syzygy

--- stockfish_vs_stockfish4.py
+++ stockfish_vs_stockfish4.py
@@ -4,11 +4,16 @@ import chess.pgn
 import chess.engine
 import random
 
+logging.basicConfig(
+    filename="uci.log",
+    level=logging.DEBUG,
+    filemode="w"
+)
+
 board = chess.Board("1k6/3r4/8/8/8/6R1/8/4K3 w - - 99 1")
 engine = chess.engine.SimpleEngine.popen_uci(r"C:\Users\lorand\Programs\stockfish\stockfish-windows-x86-64-avx2.exe")
 engine.configure({"Threads": 8, "Hash": 1024})
-
-
+engine.configure({"SyzygyPath": r"C:\Users\lorand\Programs\syzygy"})
 
 move_number = 1
 score = []
@@ -17,7 +22,7 @@ while not board.is_game_over():
     # white (Stockfish) makes a move
     engine_result = engine.analyse(board, chess.engine.Limit(time=0.1))
     best_move = engine_result["pv"][0]
-
+    #print(engine_result['score'])
     score.append(engine_result['score'].white().score())
     board.push(best_move)
     # engine.quit()
